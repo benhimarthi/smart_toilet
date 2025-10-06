044b9deb952a47c7dcc0953047b2a2fc0f03315d
benhimarthi18 <benhimarthi18@gmail.com>
2025-10-06 16:21:39 +0000

    _implementation_backend_formular_v.0.0.5

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 // Function to send an email
 const sendEmail = async (to: string, subject: string, html: string) => {
@@ -38,4 +40,4 @@ const sendEmail = async (to: string, subject: string, html: string) => {
   }
 };
 
-export { app, db, sendEmail };
+export { app, db, storage, sendEmail };
